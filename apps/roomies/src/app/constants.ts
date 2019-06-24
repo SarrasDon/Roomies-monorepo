3e14e50f9bfa58d1e23257c74beb10c45b8979da
SARRAS DONATOS <sarrasdon@gmail.com>
2019-06-24 23:44:39 +0300

    netlify routes

--- apps/roomies/src/app/constants.ts
+++ apps/roomies/src/app/constants.ts
@@ -1,4 +1,4 @@
 import { environment } from '../environments/environment';
-const production_url = 'https://roomies-web-server.herokuapp.com/';
+const production_url = 'api/';
 const dev_url = 'http://localhost:3333/api/';
 export const API_URL = environment.production ? production_url : dev_url;
