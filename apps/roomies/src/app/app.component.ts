5897154c6b8ace16e0d485082536e2218404fa3a
SARRAS DONATOS <sarrasdon@gmail.com>
2019-06-24 22:29:46 +0300

    init commit

--- apps/roomies/src/app/app.component.ts
+++ apps/roomies/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'roomies-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  data$ = this.service.getData();
+ 
   title = 'roomies';
+
+  /**
+   *
+   */
+  constructor(private service: AppService) {
+    
+  }
 }
