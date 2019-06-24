a5b584644f5233d64fb528595e5c6a6c32d476a3
SARRAS DONATOS <sarrasdon@gmail.com>
2019-06-25 00:01:21 +0300

    netlify routes

--- apps/roomies/src/app/constants.ts
+++ apps/roomies/src/app/constants.ts
@@ -1,4 +1,5 @@
 import { environment } from '../environments/environment';
 const production_url = 'api/';
+// test url
 const dev_url = 'http://localhost:3333/api/';
 export const API_URL = environment.production ? production_url : dev_url;
