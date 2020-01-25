971612afdcdc47ff08c17c2fb1155134f46efef9
SARRAS DONATOS <sarrasdon@gmail.com>
2020-01-25 23:13:26 +0200

    date pipe fix

--- apps/roomies/src/app/expenses/pipes/expense-date.pipe.ts
+++ apps/roomies/src/app/expenses/pipes/expense-date.pipe.ts
@@ -3,17 +3,16 @@ import { DatePipe } from '@angular/common';
 
 @Pipe({ name: 'expenseDate' })
 export class ExpenseDatePipe implements PipeTransform {
-  transform(value: Date, ...args: any[]) {
-    const today = Date.now();
-    const valueDate = new Date(value);
-    if (today - valueDate.getTime() < 60 * 1000) {
+  transform(date: Date, ...args: any[]) {
+    const now = Date.now();
+    const valueDate = new Date(date).getTime();
+
+    if (now - valueDate < 60 * 1000) {
       return 'Just now';
     }
 
-    const format =
-      today - valueDate.getTime() < 24 * 60 * 60 * 1000
-        ? 'shortTime'
-        : 'longDate';
-    return new DatePipe('en-us').transform(value, format);
+    const baseDate = new Date().setUTCHours(0, 0, 0, 0);
+    const format = baseDate < valueDate ? 'shortTime' : 'longDate';
+    return new DatePipe('en-us').transform(date, format);
   }
 }
