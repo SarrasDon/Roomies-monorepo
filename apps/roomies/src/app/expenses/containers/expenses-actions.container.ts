99d6ea93a4d5abb3b2423f7cce8a53c103ade5b1
SARRAS DONATOS <sarrasdon@gmail.com>
2020-01-26 00:05:37 +0200

    Minor file restructures

--- apps/roomies/src/app/expenses/containers/expenses-actions.container.ts
+++ apps/roomies/src/app/expenses/containers/expenses-actions.container.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { ExpenseReason } from '../../../shared/models';
-import {
-  ExpenseSelectors,
-  GetExpenseReasons,
-  CreateExpense
-} from '../../state';
+import { ExpenseReason } from '../../shared/models';
+import { CreateExpense, ExpenseSelectors, GetExpenseReasons } from '../state';
 
 @Component({
   selector: 'roomies-expenses-actions-container',
-  template:
-    '<roomies-expenses-actions [reasons]="reasons | async"  (formSubmitted)="onFormSubmitted($event)"></roomies-expenses-actions>'
+  template: `
+    <roomies-expenses-actions
+      [reasons]="reasons | async"
+      (formSubmitted)="onFormSubmitted($event)"
+    ></roomies-expenses-actions>
+  `
 })
 export class ExpensesActionsContainer implements OnInit {
   @Select(ExpenseSelectors.reasons) reasons: Observable<ExpenseReason[]>;
--- apps/roomies/src/app/expenses/containers/expenses-list.container.ts
+++ apps/roomies/src/app/expenses/containers/expenses-list.container.ts
@@ -8,9 +8,9 @@ import {
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subscription } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
-import { Expense } from '../../../shared/models';
-import { ExpensesListComponent } from '../../components';
-import { ExpenseSelectors, GetExpenses, GetExpensesCount } from '../../state';
+import { Expense } from '../../shared/models';
+import { ExpensesListComponent } from '../components';
+import { ExpenseSelectors, GetExpenses, GetExpensesCount } from '../state';
 
 @Component({
   selector: 'roomies-expenses-list-container',
