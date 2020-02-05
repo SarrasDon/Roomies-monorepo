1074628f4b4980df548fe3e70ffc3fdb872efa71
SARRAS DONATOS <sarrasdon@gmail.com>
2020-02-05 23:05:05 +0200

    path fix

--- apps/api/src/app/users/repositories/users.repository.ts
+++ apps/api/src/app/users/repositories/users.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from '../../shared/models';
+import { User } from '../../shared/Models';
 import { EntityRepository } from '../../shared/generics';
 
 @Injectable()
