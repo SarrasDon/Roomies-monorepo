a0ca23d88e8af4deea32b6442ccbccf0156952b1
Donatos Sarras <sarrasdon@gmail.com>
2020-11-01 20:35:38 +0200

    chore(workspace): workspace update

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  collectCoverage: true,
-  coverageReporters: ['html']
+  projects: [
+    '<rootDir>/apps/roomies',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/expenses.contracts',
+    '<rootDir>/libs/shared.data',
+    '<rootDir>/libs/user.contracts',
+  ],
 };
--- libs/expenses.contracts/jest.config.js
+++ libs/expenses.contracts/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'expenses.contracts',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/expenses.contracts'
+  coverageDirectory: '../../coverage/libs/expenses.contracts',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'expenses.contracts',
 };
