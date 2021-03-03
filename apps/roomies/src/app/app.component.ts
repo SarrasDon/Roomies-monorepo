b7cf74c4240a59345183b51044f2ad616a678c03
Donatos Sarras <donatos.sarras@oddschecker.com>
2021-03-03 15:14:50 +0200

    feat(app version): access app version from package json

--- apps/roomies/src/app/app.component.ts
+++ apps/roomies/src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { version } from '../../../../package.json'
 @Component({
   selector: 'roomies-root',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(version);
+
+  }
+}
