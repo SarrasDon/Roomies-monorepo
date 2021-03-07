39d40d3fd79fc8e415a0de8cdaaab08ad4713fb8
Donatos Sarras <donatos.sarras@oddschecker.com>
2021-03-07 20:26:56 +0200

    fix(header): re added upload img version

--- apps/roomies/src/app/core/components/header/header.component.ts
+++ apps/roomies/src/app/core/components/header/header.component.ts
@@ -1,12 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
-import {
-  AuthState,
-  getCurrentUser,
-  getIsLoggenIn,
-  logout
-} from '../../../auth/store';
+import { filter, map, take } from 'rxjs/operators';
+import { AuthState, getCurrentUser, getIsLoggenIn, logout, updateUserAvatar } from '../../../auth/store';
+import { CloudinaryService } from '../../services';
+
 
 @Component({
   selector: 'roomies-header',
@@ -25,9 +22,20 @@ export class HeaderComponent {
 
   constructor(
     private store: Store<AuthState>,
+    private cloudinary: CloudinaryService
   ) { }
 
-
+  onUploadImg() {
+    this.cloudinary.uploadResult
+      .pipe(
+        filter((res) => res.event && res.event === 'success'),
+        take(1)
+      )
+      .subscribe(({ info }) => {
+        this.store.dispatch(updateUserAvatar({ avatarUrl: info.secure_url }));
+      });
+    this.cloudinary.openWidget();
+  }
   onLogout() {
     this.store.dispatch(logout());
   }
