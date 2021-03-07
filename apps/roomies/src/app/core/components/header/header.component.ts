8141529ccb7d2621440d3c5a4d627f98a55aa5ee
Donatos Sarras <donatos.sarras@oddschecker.com>
2021-03-07 18:33:36 +0200

    fix(header): used css variables for avatar url

--- apps/roomies/src/app/core/components/header/header.component.ts
+++ apps/roomies/src/app/core/components/header/header.component.ts
@@ -1,15 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { from, Observable, of } from 'rxjs';
-import { filter, map, switchMap, take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import {
   AuthState,
   getCurrentUser,
   getIsLoggenIn,
-  logout,
-  updateUserAvatar,
+  logout
 } from '../../../auth/store';
-import { CloudinaryService } from '../../services';
 
 @Component({
   selector: 'roomies-header',
@@ -17,48 +14,21 @@ import { CloudinaryService } from '../../services';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   isLoggedIn$ = this.store.pipe(select(getIsLoggenIn));
   user$ = this.store.pipe(select(getCurrentUser));
-  avatar$: Observable<{ default: boolean; content: string }>;
+  avatar$ = this.user$.pipe(
+    map((user) =>
+      user && `url('${user.avatarUrl}')`
+    ),
+  );
 
   constructor(
     private store: Store<AuthState>,
-    private cloudinary: CloudinaryService
   ) { }
 
-  ngOnInit() {
-    this.avatar$ = this.user$.pipe(
-      map((user) =>
-        user && user.avatarUrl
-          ? { content: user.avatarUrl, default: false }
-          : { content: '/assets/images/default-avatar.png', default: true }
-      ),
-      switchMap((item) =>
-        item.default ? of(item) : this.getAvatar(item.content)
-      )
-    );
-  }
-
-  onUploadImg() {
-    this.cloudinary.uploadResult
-      .pipe(
-        filter((res) => res.event && res.event === 'success'),
-        take(1)
-      )
-      .subscribe(({ info }) => {
-        this.store.dispatch(updateUserAvatar({ avatarUrl: info.secure_url }));
-      });
-    this.cloudinary.openWidget();
-  }
 
   onLogout() {
     this.store.dispatch(logout());
-  }
-
-  getAvatar(url: string) {
-    return from(
-      this.cloudinary.getAvatarImg(url)
-    ).pipe(map((content) => ({ default: false, content })));
   }
 }
