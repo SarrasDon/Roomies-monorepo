e36ae1714062fc0e47f45f1d99e274a12b382ee7
Donatos Sarras <donatos.sarras@oddschecker.com>
2021-03-08 15:26:33 +0200

    refactor(totals): reverted monthly expenses, refactored totals with css variables

--- apps/roomies/src/app/expenses/components/totals/totals.component.ts
+++ apps/roomies/src/app/expenses/components/totals/totals.component.ts
@@ -1,18 +1,17 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { delay, map, publish, refCount, tap } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
 import { getUserImageDict } from '../../../auth/store';
 import {
   TOTAL_COUNTER_AFTER_DIALOG_CLOSED_DELAY,
-  TOTAL_COUNTER_WAIT_DOWN_DELAY,
+  TOTAL_COUNTER_WAIT_DOWN_DELAY
 } from '../../expenses.config';
 import {
   getTotals,
-  getTotalsForMonth,
   selectBalanceWithSign,
   selectTotalsWithNames,
-  TotalState,
+  TotalState
 } from '../../store';
 
 @Component({
@@ -22,34 +21,25 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TotalsComponent implements OnInit {
-  date = new Date();
   totals$ = this.store.pipe(select(selectTotalsWithNames));
-  balanceWithSign$ = this.store.pipe(
+
+  balance$ = this.store.pipe(
     select(selectBalanceWithSign),
-    publish(),
-    refCount()
-  );
-  balance$ = this.balanceWithSign$.pipe(
     delay(
       TOTAL_COUNTER_AFTER_DIALOG_CLOSED_DELAY + TOTAL_COUNTER_WAIT_DOWN_DELAY
-    )
+    ),
+    map(({ amount, sign }) => ({
+      amount: amount,
+      color: sign,
+      width: this.countDigits(amount)
+    }))
   );
 
-  balanceColor$ = this.balanceWithSign$.pipe(
-    map((balance) => this.calculatebalanceClass(balance))
-  );
-  balanceWidth$ = this.balanceWithSign$.pipe(
-    map(({ amount }) => this.countDigits(amount))
-  );
   userImagesDict$ = this.store.pipe(select(getUserImageDict));
 
   showDataLabel$ = this.breakpointObserver
     .observe([Breakpoints.XSmall, Breakpoints.HandsetPortrait])
     .pipe(map((state) => !state.matches));
-
-  readonly colorScheme = {
-    domain: ['var(--chart-color-1)', 'var(--chart-color-2)'],
-  };
 
   constructor(
     public breakpointObserver: BreakpointObserver,
@@ -57,13 +47,7 @@ export class TotalsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const month = this.date.getMonth();
-    const year = this.date.getFullYear();
-    this.store.dispatch(getTotalsForMonth({ month, year }));
-  }
-
-  calculatebalanceClass(balance: { amount: number; sign: string }) {
-    return balance ? balance.sign : ''
+    this.store.dispatch(getTotals());
   }
 
   countDigits(amount: number) {
