f9af1f530f891b1a006acb64926d301afefc12d0
Donatos Sarras <sarrasdon@gmail.com>
2021-08-23 21:17:27 +0300

    fix(material module): cleanup material module

--- apps/roomies/src/app/shared/material/material.module.ts
+++ apps/roomies/src/app/shared/material/material.module.ts
@@ -1,65 +1,41 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
   imports: [
     MatInputModule,
-    MatCardModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
     MatIconModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatTabsModule,
     MatMenuModule,
-    MatExpansionModule,
     MatRadioModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatDividerModule,
     MatSnackBarModule,
-    MatTooltipModule,
-    MatDialogModule
+    MatDialogModule,
   ],
   exports: [
-    MatTabsModule,
     MatInputModule,
-    MatCardModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
     MatIconModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
     MatMenuModule,
-    MatExpansionModule,
     MatRadioModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatDividerModule,
     MatSnackBarModule,
-    MatTooltipModule,
-    MatDialogModule
+    MatDialogModule,
   ],
-  providers: [MatDatepickerModule]
+  providers: [MatDatepickerModule],
 })
 export class MaterialModule {}
